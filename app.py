d0b94082e80d2be533f94dcdc7062f113988ea4d
AdminX <Vickky28091995@mail.ru>
2022-10-25 21:41:40 +0300

    3.Добавили поля в таблицу

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///shop.db'
+db = SQLAlchemy
+
+class Item(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = 0
+    price = 0
+    isactive = 0
+    description = 0
+    picture = 0
 
 @app.route('/')
 def index():
