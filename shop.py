674b40c9276d4a77072297cc45704120b4157619
AdminX <Vickky28091995@mail.ru>
2022-10-27 00:17:59 +0300

    4.Наполнила страницу дополнение

--- shop.py
+++ shop.py
@@ -3,15 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///shop.db'
-db = SQLAlchemy
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 class Item(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    title = 0
-    price = 0
-    isactive = 0
-    description = 0
-    picture = 0
+    title = db.Column(db.String(100),nullable=False)
+    price = db.Column(db.Integer,nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    #picture = 0
+    isactive = db.Column(db.Boolean,default=True)
+
+#db.create_all()
 
 @app.route('/')
 def index():
@@ -20,6 +23,10 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 @app.route('/addition')
 def addition():
